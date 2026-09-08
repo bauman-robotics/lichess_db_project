8eece25e0ad02bb6106efa450870c73d131266d5
baumanrobotics <baumanrobotics@gmail.com>
2026-09-08 17:05:59 +0300

    change port

--- run_web.py
+++ run_web.py
@@ -14,4 +14,4 @@ from web_app.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5555)
